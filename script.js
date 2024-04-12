230d0a0bb84fb6861195ea78a36980ddb1c49166
wingfam <supmangcua970@gmail.com>
2024-04-12 20:39:16 +0700

    Wrap GameBoard factory function inside an IIFE

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 /*
-- I need a GameBoard factory function that can be use
-to create one instance of the gameboard.
-- The board has 3 row and 3 column.
-- Each row and column will be store in a 2d array.
-- I can use for loop for row to loop through each row 
-and column, then store them in an array.
-- Create a print function to print out the board to console.
+
 */
 
-function GameBoard() {
+const GameBoard = (function () {
     const rows = 3;
     const columns = 3;
     let board = [];
@@ -24,9 +18,11 @@ function GameBoard() {
         }
     }
 
+    const getBoard = () => board;
+
     const printBoard = () => {
         console.log(board);
     };
 
-    return { board, printBoard }
-}
+    return { getBoard, printBoard }
+}) ();
