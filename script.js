9f5fb9884b6a7dda4849ad90c064190f6af5e473
wingfam <supmangcua970@gmail.com>
2024-04-16 16:20:44 +0700

    - Create check when player win a match condition. - Implement a do while loop for playing one match. - Implement a do while loop for playing in one round.

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ TODO:
 6. [X] Create a GameController factory function that responsible for controlling the 
    flow and state of the game's turn.
 7. [X] Implement a win condition after one round and give one score to winner. 
+7.1. [] Give board a blank slate after declare a winner of that round.
+8. [] After 3 round, declare winner of that match.
+9. [] Implement a tie condition after one round.
 */
 
 function Cell() {
@@ -87,6 +90,13 @@ function checkWinOneRound(playerSymbol, board) {
   return isWin;
 }
 
+function checkPlayerWinAMatch(player1, player2) {
+  const playerOneScore = player1.getScore();
+  const playerTwoScore = player2.getScore();
+  if (playerOneScore > playerTwoScore) return player1.name;
+  if (playerOneScore < playerTwoScore) return player2.name;
+}
+
 const GameBoard = (function () {
   const rows = 3;
   const columns = 3;
@@ -128,6 +138,7 @@ const GameBoard = (function () {
 
 function GameController(playerOneName, playerTwoName) {
   const board = GameBoard;
+  const totalRoundPlay = 3;
 
   const player1 = createPlayer(playerOneName, "X");
   const player2 = createPlayer(playerTwoName, "O");
@@ -147,41 +158,69 @@ function GameController(playerOneName, playerTwoName) {
   };
 
   const printRoundMessage = () => {
+    console.log("A new round has started.");
     console.log(`Now it's ${currentPlayer.name}'s turn`);
     board.printBoard();
   };
 
-  const playOneRound = (row, column) => {
-    board.placeSymbol(currentPlayer.symbol, row, column);
-    console.log(`Player ${currentPlayer.name}'s symbol has been placed`);
-
-    const winCondition = checkWinOneRound(currentPlayer.symbol, board);
-
-    if (winCondition) {
-      currentPlayer.giveScore();
-      console.log(`${currentPlayer.name} has won this round!`);
-      console.log(
-        `${currentPlayer.name}'s total score is: ${currentPlayer.getScore()}`
-      );
-    } else {
-      switchPlayerTurn();
-      printRoundMessage();
-    }
+  const printMatchMessage = () => {
+    console.log("This is a 2 of out 3 Tic-Tac-Toe match.");
+    printRoundMessage();
   };
 
-  printRoundMessage();
+  const playMatch = () => {
+    let currentRound = 0;
+    do {
+      // TODO: Find a way to implement invoke playing one round
+      // inside this do while loop.
+      playOneRound();
+      currentRound++;
+    } while (currentRound < totalRoundPlay);
 
-  return { playOneRound, getCurrentPlayer };
+    const winner = checkPlayerWinAMatch(player1, player2);
+    console.log(`Congratulation! ${winner} has won the match.`);
+  };
+
+  function playOneRound() {
+    let winOneCondition = false;
+
+    do {
+      let inputRow = prompt("Type row you want to place your symbol.");
+      let inputCol = prompt("Type column you want to place your symbol.");
+
+      board.placeSymbol(currentPlayer.symbol, inputRow, inputCol);
+      console.log(`Player ${currentPlayer.name}'s symbol has been placed`);
+      winOneCondition = checkWinOneRound(currentPlayer.symbol, board);
+
+      if (winOneCondition) {
+        board.printBoard();
+        currentPlayer.giveScore();
+        console.log(`${currentPlayer.name} has won this round!`);
+        console.log(
+          `${currentPlayer.name}'s total score is: ${currentPlayer.getScore()}`
+        );
+      } else {
+        switchPlayerTurn();
+        board.printBoard();
+        console.log(`It's ${currentPlayer.name}'s turn`);
+      }
+    } while (winOneCondition === false);
+
+    switchPlayerTurn();
+    printRoundMessage();
+  }
+
+  printMatchMessage();
+
+  return { getCurrentPlayer, playOneRound, playMatch };
 }
 
 const game = GameController("Minh", "BOT");
+game.playMatch();
 
-// game.playOneRound(1, 0);
-// game.playOneRound(1, 1);
-// game.playOneRound(0, 1);
+// First round: Minh won
 // game.playOneRound(0, 0);
-// game.playOneRound(2, 2);
+// game.playOneRound(1, 0);
+// game.playOneRound(0, 1);
+// game.playOneRound(1, 1);
 // game.playOneRound(0, 2);
-// game.playOneRound(2, 0);
-// game.playOneRound(1, 2);
-// game.playOneRound(2, 1);
