24a0613217d4a7d57d4e16daae20a0ed43ec79fe
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2024-06-15 23:16:30 +0700

    - added a check that keep player from playing in spots that already have symbol in them. - added tie condition

--- script.js
+++ script.js
@@ -4,61 +4,36 @@ TODO:
 Try to get value at the cell that the player is trying to place their symbol.
 If it already had a value do nothing, else place current player's symbol.
 - Add a tie condition
-*/ 
+*/
 
-function Cell() {
-  let value = " ";
-
-  function addSymbol(playerSymbol) {
-    value = value.replace(value, playerSymbol);
-  }
-
-  const getValue = () => value;
-
-  return { addSymbol, getValue };
-}
-
-function createPlayer(name, symbol) {
+function Player(name, symbol) {
   let score = 0;
   const getScore = () => score;
   const giveScore = () => score++;
   return { name, symbol, getScore, giveScore };
 }
 
-const DisplayController = (function () {
-  const playerTurnLegend = document.querySelector("legend");
+function Cell() {
+  let value = "";
 
-  const insertPlayerSymbol = (tdTag, playerSymbol) => {
-    tdTag.textContent = playerSymbol;
-  };
+  const getValue = () => value;
 
-  const changePlayerTurnText = (activePlayer) => {
-    const regex = /[^\s]+/; // Match any character until the first white space.
-    playerTurnLegend.textContent = playerTurnLegend.textContent.replace(
-      regex,
-      activePlayer.name
-    );
-  };
+  function addSymbol(playerSymbol) {
+    value = value.replace(value, playerSymbol);
+  }
 
-  const redrawBoard = () => {
-    const tdTags = document.querySelectorAll("td");
-    tdTags.forEach((tag) => {
-      tag.textContent = "";
-    });
-  };
-
-  return { insertPlayerSymbol, changePlayerTurnText, redrawBoard };
-})();
+  return { addSymbol, getValue };
+}
 
 const GameBoard = (function () {
+  let board = [];
   const rows = 3;
   const columns = 3;
   const maximumPlacement = rows * columns;
-  let board = [];
 
+  const getBoard = () => board;
   const getColumn = () => columns;
   const getMaxPlacement = () => maximumPlacement;
-  const getBoard = () => board;
 
   for (let i = 0; i < rows; i++) {
     // For each row, asign an empty array
@@ -79,14 +54,19 @@ const GameBoard = (function () {
     );
   };
 
-  const placeSymbol = (playerSymbol, row, column) => {
-    board[row][column].addSymbol(playerSymbol);
+  const placeSymbol = (activePlayerSymbol, row, column) => {
+    const boardWithValues = getBoardWithValues();
+    if (!boardWithValues[row][column]) {
+      board[row][column].addSymbol(activePlayerSymbol);
+      return true;
+    }
+    return false;
   };
 
   function clearBoard() {
     board.forEach((row) => {
       row.forEach((cell) => {
-        cell.addSymbol(" ");
+        cell.addSymbol("");
       });
     });
   }
@@ -97,15 +77,43 @@ const GameBoard = (function () {
     placeSymbol,
     getBoardWithValues,
     clearBoard,
-    getMaxPlacement,
+    getMaxPlacement
   };
+})();
+
+const DisplayController = (function () {
+  const playerTurnLegend = document.querySelector("legend");
+
+  const insertPlayerSymbol = (row, col, playerSymbol) => {
+    const selectedCell = document.querySelector(
+      "tr:nth-child(" + row + ") > td:nth-child(" + col + ")"
+    );
+    selectedCell.textContent = playerSymbol;
+  };
+
+  const changePlayerTurnText = (activePlayer) => {
+    const regex = /[^\s]+/; // Match any character until the first white space.
+    playerTurnLegend.textContent = playerTurnLegend.textContent.replace(
+      regex,
+      activePlayer.name
+    );
+  };
+
+  const redrawBoard = () => {
+    const tdTags = document.querySelectorAll("td");
+    tdTags.forEach((tag) => {
+      tag.textContent = "";
+    });
+  };
+
+  return { insertPlayerSymbol, changePlayerTurnText, redrawBoard };
 })();
 
 function GameController(playerOneName, playerTwoName) {
   const board = GameBoard;
   const displayController = DisplayController;
-  const player1 = createPlayer(playerOneName, "X");
-  const player2 = createPlayer(playerTwoName, "O");
+  const player1 = Player(playerOneName, "X");
+  const player2 = Player(playerTwoName, "O");
 
   let numberOfPlacement = 0;
   let activePlayer = player1;
@@ -131,17 +139,15 @@ function GameController(playerOneName, playerTwoName) {
   };
 
   const checkWinCondition = (playerSymbol, board) => {
-    let isWin = false;
-    const totalCol = board.getColumn();
     const boardWithValues = board.getBoardWithValues();
+    const totalCol = board.getColumn();
 
     const compareSymbol = (value) => value === playerSymbol;
 
-    /* Check for each cell in a row. Use every */
+    /* Check for each cell in a row */
     function checkRow() {
       for (let row of boardWithValues) {
         if (row.every(compareSymbol)) return true;
-        // else return false;
       }
     }
 
@@ -153,7 +159,6 @@ function GameController(playerOneName, playerTwoName) {
         });
 
         if (col.every(compareSymbol)) return true;
-        // else return false;
       }
     }
 
@@ -177,38 +182,16 @@ function GameController(playerOneName, playerTwoName) {
       if (isLinearTrue || isReverseTrue) return true;
     }
 
-    if (checkRow() || checkColumn() || checkDiagonal()) isWin = true;
-
-    return isWin;
-  };
-
-  const playGame = (inputRow, inputCol) => {
-    numberOfPlacement++;
-    board.placeSymbol(activePlayer.symbol, inputRow, inputCol);
-    let winCondition = checkWinCondition(activePlayer.symbol, board);
-
-    // Check win condition
-    if (winCondition) {
-      board.clearBoard();
-      activePlayer.giveScore();
-      displayController.redrawBoard();
+    if (checkRow() || checkColumn() || checkDiagonal()) {
       alert(activePlayer.name + " has won the match");
-    } else {
-      switchPlayerTurn();
-    }
-
-    // Check tie condition
-    // if (numberOfPlacement >= board.getMaxPlacement()) {
-    //   resetGame();
-    //   displayController.redrawBoard();
-    // }
+      return true;
+    };
   };
 
-  function addEventToGridItems() {
+  function addPlayGameEventToCells() {
     const cells = document.querySelectorAll("td");
     cells.forEach((cell) => {
       cell.addEventListener("click", function () {
-        displayController.insertPlayerSymbol(cell, activePlayer.symbol);
         playGame(cell.closest("tr").rowIndex, cell.cellIndex);
       });
     });
@@ -221,7 +204,33 @@ function GameController(playerOneName, playerTwoName) {
     });
   }
 
-  addEventToGridItems();
+  const playGame = (row, col) => {
+    const isSymbolPlaced = board.placeSymbol(activePlayer.symbol, row, col);
+    if (isSymbolPlaced) {
+      numberOfPlacement++;
+      // increase row and column by 1 because HTML table row and column index begin with 1
+      displayController.insertPlayerSymbol(row + 1, col + 1, activePlayer.symbol);
+    }
+
+    // Check win condition
+    if (checkWinCondition(activePlayer.symbol, board)) {
+      board.clearBoard();
+      activePlayer.giveScore();
+      displayController.redrawBoard();
+      numberOfPlacement = 0;
+    } else {
+      switchPlayerTurn();
+    }
+
+    // Check tie condition
+    if (numberOfPlacement >= board.getMaxPlacement()) {
+      alert("The game is tie. The game will reset");
+      resetGame();
+      numberOfPlacement = 0;
+    }
+  };
+
+  addPlayGameEventToCells();
   resetButtonEventHandler();
 
   return { getCurrentPlayer, playGame, resetGame, resetButtonEventHandler };
